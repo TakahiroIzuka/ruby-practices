2d802eed3a4cf7cad96ac52f72cb1f0b24b7f794
TakahiroIzuka <takahiroi.311@gmail.com>
2023-06-25 13:17:14 +0900

    OSのcalコマンドに合わせた挙動になるように修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,51 +3,52 @@
 require 'date'
 require 'optparse'
 
-today = Date.today
 # ターゲットの日付は、todayで初期化する
-target_year = today.year
-target_month = today.month
+year = Date.today.year
+month = Date.today.month
 
 opt = OptionParser.new
 # -yオプションで入力された年
 opt.on('-y VAL', String) do |y|
   # ターゲット年を上書き
-  target_year = y.to_i
+  year = y.to_i
   raise OptionParser::InvalidArgument.new("年は1970~2100の間で設定してください") if target_year < 1970 || 2100 < target_year
 end
 
 #-mオプションで入力された月
 opt.on('-m VAL') do |m|
   # ターゲット月を上書き
-  target_month = m.to_i
+  month = m.to_i
   raise OptionParser::InvalidArgument.new("月は1~12の間で設定してください") if target_month < 1 || 12 < target_month
 end
 
 # parseしないとopt.onのブロックが実行されないので注意
 opt.parse!(ARGV)
 
-first_date = Date.new(target_year, target_month, 1)
-last_date = Date.new(target_year, target_month, -1)
-days_in_month = []
-1.upto(last_date.day) {|day| days_in_month.push(day)}
-# 月初めが月曜日でない場合はその数だけ空文字を入れてフォーマットを調整する
-(first_date.cwday).times {
+target_month = Date.new(year, month)
+target_month_last_date = Date.new(year, month, -1)
+days_in_month = (1..target_month_last_date.day.to_i).to_a
+
+(target_month.wday).times do
   days_in_month.unshift('')
-}
+end
 
 RED = 31
-BLUE = 34
-
-# 基本、数字のカラーは赤色で出力する
-printf("%33s", "\e[#{RED}m#{target_year}\e[0m年 \e[#{RED}m#{target_month}\e[0m月")
+DEFAULT = 39
+printf("%14s", "#{target_month.strftime('%B')} #{target_month.year}")
 puts
-printf("%14s", "日 月 火 水 木 金 土")
+printf("%13s", "Su Mo Tu We Th Fr Sa")
 days_in_month.length.times do |i|
-  color = RED
-  puts if i % 7 == 0
-  # 日付がtodayと同じ時だけ、カラーを青色にする
-  color = BLUE if target_month < 10 && "#{target_year}-0#{target_month}-#{days_in_month[i]}".eql?(today.to_s)
-  color = BLUE if target_month > 10 && "#{target_year}-#{target_month}-#{days_in_month[i]}".eql?(today.to_s)
-  printf("%8s", "\e[#{color}m#{days_in_month[i]}")
+  color = DEFAULT
+  format = "%8s"
+
+  if i % 7 == 0
+    puts
+    format = "%7s"
+  end
+  color = RED if !(days_in_month[i].to_s).empty? && Date.new(target_month.year, target_month.month, days_in_month[i]) == Date.today
+  printf(format, "\e[#{color}m#{days_in_month[i]}")
 end
 puts
+puts
+

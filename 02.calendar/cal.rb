5091df9af54b97ae38e545ad4807c3061e33a09b
TakahiroIzuka <takahiroi.311@gmail.com>
2023-07-02 22:01:43 +0900

    printf -> printに修正し、全体的にコードをスッキリさせました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'date'
 require 'optparse'
+require 'debug'
 
 RED = 31
-DEFAULT = 39
 
 year = Date.today.year
 month = Date.today.month
@@ -23,37 +24,20 @@ opt.parse!(ARGV)
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 
-case first_date.strftime('%B').length
-when 3..4
-  format_month = '%15s'
-when 5..6
-  format_month = '%16s'
-when 7..8
-  format_month = '%17s'
-when 9
-  format_month = '%18s'
-end
-
-printf(format_month, "#{first_date.strftime('%B')} #{first_date.year}")
+print "#{first_date.strftime('%B')} #{first_date.year}".center(21)
 puts
 
-printf 'Su Mo Tu We Th Fr Sa'
-puts if first_date.wday > 0
-printf "#{[*Array.new(first_date.wday, '   ')].join}"
+puts 'Su Mo Tu We Th Fr Sa'
+print '   ' * first_date.wday
 
-color = DEFAULT
-(first_date..last_date).each do |date|
-  format_day = '%7s '
-  puts if date.sunday?
+(first_date..last_date).each.with_index do |date, i|
+  puts if date.sunday? && i > 0
 
+  day = date.day.to_s.rjust(2)
   if date == Date.today
-    color = RED
-    printf(format_day, "\e[#{color}m#{date.day}")
-
-    # DEFAULTで上書きしないと以後出力される文字色が全てREDで出力されてしまう
-    color = DEFAULT
+    print "\e[#{RED}m#{day}\e[0m "
   else
-    printf(format_day, "\e[#{color}m#{date.day}")
+    print "#{day} "
   end
 end
 print "\n\n"

ed07474134f26d70dfaf279d788ee9bf79ff0101
yuzuuu <takahiroi.311@gmail.com>
2023-10-17 22:24:11 +0900

    リファクタリング

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,17 +6,21 @@ require 'optparse'
 def get_each_total(files, options)
   total_line = 0
   total_word = 0
-  total_character = 0
+  total_byte = 0
   files.each do |file|
     total_line += options['l'] ? file[:text].scan(/\n/).length : 0
     total_word += options['w'] ? file[:text].split(/\s+/).length : 0
-    total_character += options['c'] ? file[:text].length : 0
+    total_byte += options['c'] ? file[:text].bytesize : 0
   end
   {
-    lines: total_line != 0 ? "#{total_line} " : '',
-    words: total_word != 0 ? "#{total_word} " : '',
-    characters: total_character != 0 ? "#{total_character} " : ''
+    line_count: total_line.positive? ? "#{total_line} " : '',
+    word_count: total_word.positive? ? "#{total_word} " : '',
+    byte_count: total_byte.positive? ? "#{total_byte} " : ''
   }
+end
+
+def output_wc(outputs, widths, file_name = '')
+  puts "#{outputs[0].rjust(widths[0])}#{outputs[1].rjust(widths[1])}#{outputs[2].rjust(widths[2])}#{file_name}"
 end
 
 options = ARGV.getopts('l', 'w', 'c')
@@ -28,51 +32,67 @@ end
 
 files = []
 ARGV.each do |file_name|
-  file = File.open(file_name)
-  text = file.read
+  text = File.read(file_name)
   files << { file_name:, text: }
-  file.close
 end
 
 if files.empty?
-  lines = 0
-  words = 0
-  characters = 0
-  while line = ARGF.gets
-    lines += 1
-    words += line.split.size
-    characters += line.size
+  # p '標準入力'
+  readlines = ARGF.readlines
+  line_count = readlines.length
+  word_count = 0
+  byte_count = 0
+  readlines.each do |line|
+    word_count += line.split.size
+    byte_count += line.size
   end
-  output_array = Array.new(2, '')
-  output_array.unshift characters.to_s if options['c']
-  output_array.unshift words.to_s if options['w']
-  output_array.unshift lines.to_s if options['l']
+
+  outputs = Array.new(2, '')
+  outputs.unshift byte_count.to_s if options['c']
+  outputs.unshift word_count.to_s if options['w']
+  outputs.unshift line_count.to_s if options['l']
 
   if options.values.count(true) == 1
-    puts output_array[0]
+    puts outputs[0]
   else
-    puts "#{output_array[0].rjust(7)}#{output_array[1].rjust(8)}#{output_array[2].rjust(8)}"
+    widths = []
+    widths.push(outputs[0].length > 7 ? outputs[0].length + 1 : 7)
+    widths.push(outputs[1].length > 7 ? outputs[0].length + 1 : 8)
+    widths.push(outputs[2].length > 7 ? outputs[0].length + 1 : 8)
+
+    output_wc(outputs, widths)
+    # puts "#{outputs[0].rjust(widths[0])}#{outputs[1].rjust(widths[1])}#{outputs[2].rjust(widths[2])}"
   end
 else
+  # p 'ファイルからの入力'
   each_total = get_each_total(files, options)
-  total_lines = each_total[:lines]
-  total_words = each_total[:words]
-  total_char = each_total[:characters]
-  total_lines_size = !total_lines.empty? ? total_lines.size + 1 : 0
-  total_words_size = !total_words.empty? ? total_words.size + 1 : 0
-  total_characters_size = !total_char.empty? ? total_char.size : 0
+  total_line = each_total[:line_count]
+  total_word = each_total[:word_count]
+  total_byte = each_total[:byte_count]
+
+  widths = []
+  widths.push(total_line.empty? || (options.values.count(true) == 1 && files.count == 1) ? 0 : total_line.size + 1)
+  widths.push(total_word.empty? || (options.values.count(true) == 1 && files.count == 1) ? 0 : total_word.size + 1)
+  widths.push(total_byte.empty? || (options.values.count(true) == 1 && files.count == 1) ? 0 : total_byte.size)
+
+  # total_line_count = total_line.empty? || options.values.count(true) == 1 ? 0 : total_line.size + 1
+  # total_word_count = total_word.empty? || options.values.count(true) == 1 ? 0 : total_word.size + 1
+  # total_byte_count = total_byte.empty? || options.values.count(true) == 1 ? 0 :  total_byte.size
 
   files.each do |file|
-    lines = options['l'] ? "#{file[:text].scan(/\n/).length} " : ''
-    words = options['w'] ? "#{file[:text].split(/\s+/).length} " : ''
-    characters = options['c'] ? "#{file[:text].length} " : ''
+    outputs = []
+    outputs.unshift options['c'] ? "#{file[:text].bytesize} " : ''
+    outputs.unshift options['w'] ? "#{file[:text].split(/\s+/).length} " : ''
+    outputs.unshift options['l'] ? "#{file[:text].scan(/\n/).length} " : ''
 
-    if files.count == 1 && options.values.count(true) == 1
-      puts "#{lines}#{words}#{characters}#{file[:file_name]}"
-    else
-      puts "#{lines.rjust(total_lines_size)}#{words.rjust(total_words_size)}#{characters.rjust(total_characters_size)}#{file[:file_name]}"
-    end
+    # outputs = Array.new(2, '')
+    # p outputs.unshift "#{file[:text].bytesize} " if options['c']
+    # p outputs.unshift "#{file[:text].split(/\s+/).length} " if options['w']
+    # p outputs.unshift "#{file[:text].scan(/\n/).length} " if options['l']
+
+    output_wc(outputs, widths, file[:file_name])
+    # puts "#{outputs[0].rjust(widths[0])}#{outputs[1].rjust(widths[1])}#{outputs[2].rjust(widths[2])}#{file[:file_name]}"
   end
 
-  puts "#{total_lines.rjust(total_lines_size)}#{total_words.rjust(total_words_size)}#{total_char.rjust(total_characters_size)}total" if files.count >= 2
+  puts "#{total_line.rjust(widths[0])}#{total_word.rjust(widths[1])}#{total_byte.rjust(widths[2])}total" if files.count >= 2
 end

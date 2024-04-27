2e3403fafa7e2e4fda3699ab88b23cdf9d496da2
TakahiroIzuka <takahiroi.311@gmail.com>
2024-04-27 16:15:34 +0900

    gameクラスにplayメソッドを追加し、マークを登録できるようにする

---
+++ 06.bowling_object/bowling.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative './class/game'
+
+mark = ARGV[0]
+marks = mark.split(',')
+
+game = Game.new
+game.play(marks)
+game.score
---
+++ 06.bowling_object/class/game.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require_relative './frame'
+require_relative './last_frame'
+require_relative './shot'
+
+# Gameクラスを定義
+class Game
+  def initialize
+    @frames = []
+  end
+
+  def play(marks)
+    # marksのチェックと成形
+    marks = add_zero_after_strike(marks)
+    check_size_of_marks(marks)
+    check_content_of_marks(marks)
+    formatted_marks = formatted_marks(marks)
+
+    # shot, frameの生成
+    formatted_marks.each_with_index do |marks_per_frame, index|
+      shots = marks_per_frame.map { |mark| Shot.new(mark) }
+
+      if index >= 9
+        @frames << LastFrame.new(*shots) if index == 9
+        next
+      end
+      @frames << Frame.new(*shots)
+    end
+  end
+
+  def score
+    # TODO: scoreの計算
+  end
+
+  private
+
+  def add_zero_after_strike(marks)
+    marks.each_with_index do |mark, index|
+      next if index >= 18
+
+      marks.insert(index + 1, 0) if mark == 'X'
+    end
+  end
+
+  def check_size_of_marks(marks)
+    raise 'Number of marks is invalid' unless marks.size == 20 || marks.size == 21
+  end
+
+  def check_content_of_marks(marks)
+    marks.each_with_index do |mark, index|
+      raise "Invalid mark: 'X' can marked only first shot in the frame" if index < 18 && index.odd? && mark == 'X'
+      raise "Invalid mark: mark use only 'X', integer 0 to 9" unless mark == 'X' || mark.to_i.between?(0, 9)
+    end
+  end
+
+  def formatted_marks(marks)
+    formatted_marks = marks.each_slice(2).map { |array| array }
+    if formatted_marks.size == 11
+      formatted_marks[9].concat(formatted_marks[10])
+      formatted_marks.delete_at(10)
+    end
+
+    formatted_marks
+  end
+end

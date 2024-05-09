5afde70a20b130cec309594cddfef10a7e98e4b6
TakahiroIzuka <takahiroi.311@gmail.com>
2024-05-09 20:24:24 +0900

    total scoreのvalidateメソッドをリファクタリング

--- 06.bowling_object/lib/last_frame.rb
+++ 06.bowling_object/lib/last_frame.rb
@@ -27,19 +27,13 @@ class LastFrame < Frame
   private
 
   def max_score
-    return 30 if first_shot.mark == 'X' && second_shot.mark == 'X'
-    return 20 if first_shot.mark == 'X' || @shots[0..1].sum(&:score) == MAX_SCORE
+    return 30 if @shots.size >= 2 && first_shot.mark == 'X' && second_shot.mark == 'X'
+    return 20 if @shots.size >= 1 && first_shot.mark == 'X' || @shots[0..1].sum(&:score) == MAX_SCORE
 
     MAX_SCORE
   end
 
   def validate_second_shot(shot)
     raise 'Invalid shot (X can mark only after X)' if first_shot.mark != 'X' && shot.mark == 'X'
-  end
-
-  def validate_total_two_score(shot)
-    return if first_shot.mark == 'X'
-
-    super
   end
 end

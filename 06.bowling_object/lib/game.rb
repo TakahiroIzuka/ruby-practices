00e90c87ad9e881b7c18519fb05361374200c4d4
TakahiroIzuka <takahiroi.311@gmail.com>
2024-05-21 19:58:14 +0900

    gameクラスをリファクタリング

--- 06.bowling_object/lib/game.rb
+++ 06.bowling_object/lib/game.rb
@@ -10,44 +10,28 @@ class Game
   end
 
   def play(marks)
-    shots = Shot.shot_factory(marks)
-    @frames.each do |frame|
+    @frames.each.with_index(1) do |frame, current_frame|
       3.times do
-        break if frame.full? || shots.empty?
+        break if frame.full? || marks.empty?
 
-        frame.set(shots.shift)
+        shot = Shot.new(marks.shift)
+        frame.set(shot)
+        frame.set(Shot.new('0')) if shot.strike? && current_frame < 10
       end
     end
 
-    score
+    @frames.sum(&:score)
   end
 
   private
 
-  def score
-    sum = 0
-    return sum if @frames.empty?
-
-    @frames.each_with_index do |frame, index|
-      check_set(frame, index)
-      sum += frame.score
-    end
-
-    sum
-  end
-
   def frame_factory
     prev_frame = nil
-    frames = (1..10).map do |index|
-      frame = index == 1 ? LastFrame.new : Frame.new(prev_frame)
+    frames = Array.new(10) do |index|
+      frame = index.zero? ? LastFrame.new : Frame.new(prev_frame)
       prev_frame = frame
     end
 
     frames.reverse
-  end
-
-  def check_set(frame, index)
-    raise 'Invalid frame (Processing frame is not full)' if index.zero? && !frame.full?
-    raise 'Invalid frame (Next frame is not full)' if !frame.next_frame.nil? && !frame.next_frame.full?
   end
 end
